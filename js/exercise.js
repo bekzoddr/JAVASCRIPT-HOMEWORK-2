cfcaa6e06a9949ed8ff8d9ce949fdf5009bd3839
bekzoddr <sodiqov.bekzoddr@gmail.com>
2023-12-10 23:40:03 +0500

    operators

---
+++ js/exercise.js
@@ -0,0 +1,173 @@
+// Boolean-1
+
+// let A = 1;
+
+// let res = A > 0;
+
+// console.log(res);
+
+// Boolean-2
+
+// let A = 5;
+// let B = -3;
+
+// let res = A > 0 && B <= -2;
+
+// console.log(res);
+
+// Boolean-3
+
+// let A = 13;
+// let B = 15;
+// let C = 17;
+
+// let res = A <= B && B <= C;
+
+// console.log(res);
+
+// Boolean-4
+
+// let A = 11;
+// let B = 7;
+
+// let res = (A + B) % 2 == 0;
+
+// console.log(res);
+
+
+
+// Boolean-5
+
+// let A = 30;
+// let B = 3;
+
+// let res = (A + B) % 2 != 0;
+
+// console.log(res);
+
+// Boolean-6
+
+// let A = 30;
+// let B = 11;
+
+// let res = (A % 2 == 0 && B % 2 == 1) || (A % 2 == 1 && B % 2 == 0);
+
+// (A + B) % 2 == 0;
+
+// console.log(res);
+
+// console.log(res);
+
+// Boolean-7
+
+// let A = 29;
+// let B = 17;
+// let C = 4;
+
+// let res = (A + B + C) % 2 == 1;
+
+// console.log(res);
+
+// Boolean-8
+
+// let A = 22;
+// let B = 8;
+// let C = 10;
+
+// let res = (A + B + C) % 2 == 0;
+
+// console.log(res);
+
+// Boolean-9
+
+// let A = -1;
+// let B = 12;
+// let C = 20;
+
+// let res = A > 0 || B > 0 && C > 0;
+
+// console.log(res);
+
+// Boolean-10
+
+// let AB = 10;
+
+// let res = AB % 2 == 0;
+
+// console.log(res);
+
+// Boolean-11
+
+// let abc = 123;
+
+// let res = abc % 2 == 1
+
+// console.log(res);
+
+// Boolean-12
+
+
+
+// Boolean-13
+
+
+
+// Boolean-14
+
+
+
+// Boolean-15
+
+               
+
+// OPERATORS
+
+// IFODA-1
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y1 = 1 / Math.pow(A, 2);
+
+// let Y2 = Math.pow(B / 10, 2);
+
+// let Y3 = Math.sqrt(A + C);
+
+// let Y = Y1 - Y2 * Y3;
+
+// console.log(Y);
+
+// IFODA-2
+
+
+
+// IFODA-3
+
+
+
+// IFODA-4
+
+
+
+// IFODA-5
+
+
+
+// IFODA-6
+
+
+
+// IFODA-7
+
+
+
+// IFODA-8
+
+
+
+// IFODA-9
+
+
+
+// IFODA-10
